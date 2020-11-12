41e941c30eedfb5809088b22c0794241191374fa
natethegrate <njbutler99@gmail.com>
2020-11-12 11:56:51 -0800

    slight adjustment to naming images in imageprep, and added dataloader class

--- imageprep.py
+++ imageprep.py
@@ -1,9 +1,11 @@
+import glob
 from PIL import Image
 
 CROPSIZE = 12 # cropsize = x produces 12x12 cropped images
 im = Image.open("./images/full_images/test.tif")
 
-width, height = im.size
+width, height = 1000,1000#im.size
+k = 0
 # rows
 for i in range(0, width - CROPSIZE, CROPSIZE):
     # columns
@@ -11,5 +13,8 @@ for i in range(0, width - CROPSIZE, CROPSIZE):
         # crop image with square of (i,j), (i+cropsize, j+cropsize)
         cropped = im.crop((i, j, i+CROPSIZE, j+CROPSIZE))
         # cropped images have format (y, x)
-        cropped.save("./images/cropped_images/croptest_" + str(i) + "," + str(j) +"_.tif")
-
+        #cropped.save("./images/cropped_images/croptest_" + str(i) + "," + str(j) +"_.tif")
+       
+        #just doing images in order for now
+        cropped.save("./images/cropped_images/croptest_" + str(k)+ "_.tif")
+        k += 1
---
+++ loaddata.py
@@ -0,0 +1,35 @@
+import pandas
+from torch.utils.data.dataset import Dataset
+
+
+# credit to this git repo: https://github.com/utkuozbulak/pytorch-custom-dataset-examples
+class ImageDataset(Dataset):
+    def __init__(self, csv_path):
+        # Transforms
+        self.to_tensor = transforms.ToTensor()
+        # Read the csv file
+        self.data_info = pd.read_csv(csv_path, header=None)
+        # First column contains the image paths
+        self.image_arr = np.asarray(self.data_info.iloc[:, 0])
+        # Second column is the labels
+        self.label_arr = np.asarray(self.data_info.iloc[:, 1])
+
+        # Calculate len
+        self.data_len = len(self.data_info.index)
+
+    def __getitem__(self, index):
+        # Get image name from the pandas df
+        single_image_name = self.image_arr[index]
+        # Open image as rgb
+        img_as_img = Image.open(single_image_name).convert("RGB")
+         # Transform image to tensor
+        img_as_tensor = self.to_tensor(img_as_img)
+
+        # Get label(class) of the image based on the cropped pandas column
+        single_image_label = self.label_arr[index]
+
+        return (img_as_tensor, single_image_label)
+
+    def __len__(self):
+        return self.data_len
+
